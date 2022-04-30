b20c988b17802cbe0309ae642f7f84c8b7a75216
Ivan Ertel <alanertel2275@gmail.com>
2022-04-29 21:46:45 -0300

    porongoApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { RevistaComponent } from './component/revista/revista.component';
+import { ContactoComponent } from './component/contacto/contacto.component';
+import { SearchComponent } from './component/search/search.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AutorComponent } from './component/autor/autor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    RevistaComponent,
+    ContactoComponent,
+    SearchComponent,
+    FooterComponent,
+    AutorComponent
   ],
   imports: [
     BrowserModule,
